2475fd350208b8734d04846ab71c55c7925dd1ed
angeldiazcalleja <105804506+angeldiazcalleja@users.noreply.github.com>
2022-08-28 17:50:40 +0200

    Add files via upload

---
+++ client-list-business.js
@@ -0,0 +1,11 @@
+
+function printClientsAccounts() {
+    const clients = getClients();
+    const ul = document.createElement("ul");
+    for (let client of clients) {
+      const element = getClientElement(client);
+      ul.appendChild(element);
+    }
+  
+    document.getElementById("root").appendChild(ul);
+  }
